70ac6cb206dd36b16b37735ee4a6d70ad43b6e67
Sam Calvert <sam.clvrt@gmail.com>
2014-07-07 19:49:42 -0700

    init

---
+++ diet.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+  "github.com/gorilla/mux"
+  "net/http"
+  "log"
+  "io/ioutil"
+  "encoding/xml"
+  "regexp"
+  "strconv"
+)
+
+var pointsRegexp = regexp.MustCompile("(\\d+) points")
+
+type RSS struct {
+  XMLName xml.Name `xml:"rss"`
+  Items Items `xml:"channel"`
+}
+
+type Items struct {
+  XMLName xml.Name `xml:"channel"`
+  ItemList []Item `xml:"item"`
+}
+
+type Item struct {
+  Title string `xml:"title"`
+  Link string `xml:"link"`
+  Description string `xml:"description"`
+  Comments string `xml:"comments"`
+}
+
+func main() {
+  r := mux.NewRouter()
+  r.HandleFunc("/feeds/hn/{min_points:[0-9]+}", hn)
+
+  http.Handle("/", r)
+
+  log.Println("Listening...")
+  http.ListenAndServe(":3000", nil)
+}
+
+func hn(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  min_points, _ := strconv.Atoi(params["min_points"])
+
+  resp, _ := http.Get("https://news.ycombinator.com/rss")
+  defer resp.Body.Close()
+  contents, _ := ioutil.ReadAll(resp.Body)
+
+  var i RSS
+  err := xml.Unmarshal(contents, &i)
+  if err != nil {
+  }
+
+  var validItems []Item
+  for _,item := range i.Items.ItemList {
+    resp, _ := http.Get(item.Comments)
+    defer resp.Body.Close()
+    html, _ := ioutil.ReadAll(resp.Body)
+    matches := pointsRegexp.FindStringSubmatch(string(html))
+    if len(matches) >= 2 {
+      points, _ := strconv.Atoi(matches[1])
+      if points >= min_points {
+        validItems = append(validItems, item)
+      }
+    }
+  }
+
+  i.Items.ItemList = validItems
+
+  data, _ := xml.MarshalIndent(i, "", "    ")
+  w.Write(data)
+}
